c7f8f7d254d29dbcc544a74599708c68e1176bc4
Niku2420 <67705549+Niku2420@users.noreply.github.com>
2022-07-28 22:44:05 +0530

    Time: 77 ms (50.19%), Space: 14.4 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,4 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s)==Counter(t)
+        D1={}
+        D2={}
+        for i in s:
+            if i not in D1:
+                D1[i]=1
+            else:
+                D1[i]+=1
+        for i in t:
+            if i not in D2:
+                D2[i]=1
+            else:
+                D2[i]+=1
+        return D1==D2
         
