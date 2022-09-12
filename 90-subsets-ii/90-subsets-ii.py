e554aa60f0840f36a12f8d63d3c69af2f0839316
Niku2420 <67705549+Niku2420@users.noreply.github.com>
2022-09-13 00:08:58 +0530

    Time: 80 ms (14.52%), Space: 14.1 MB (49.35%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums=sorted(nums)
+        subset=[[]]
+        for n in nums:
+            for i in range(len(subset)):
+                currentSubset=subset[i]
+                newSubset=currentSubset+[n]
+                if newSubset not in subset:
+                    subset.append(newSubset)
+        return subset
+        
+        #[1,2,2]
+        
+        # [[],[1],[2],[2,1],[2,2],[2,2,1]]
+        
+        #[4,4,4,1,4]
+        # [[],[4],[4,4],[4,4,4],[1],[1,4],[1,4,4],[4,4,4,1],[4,4,4,4],        
+        
+                
