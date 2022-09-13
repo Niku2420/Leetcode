4de883cd2c165cc2a30c9bcccac29da7ff8e0201
Niku2420 <67705549+Niku2420@users.noreply.github.com>
2022-09-13 22:49:35 +0530

    Time: 116 ms (16.46%), Space: 17.7 MB (30.84%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:    
+        try:
+            first=head
+            second=head.next
+            while first!=second:
+                first=first.next
+                second=second.next.next
+            return first
+        except:
+            return False
+            
+    
+    
+        
+        
