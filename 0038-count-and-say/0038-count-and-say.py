548c46d797fb2d46e9084e9d9ed4fa8340bc3af7
Niku2420 <67705549+Niku2420@users.noreply.github.com>
2022-10-18 23:23:14 +0530

    Time: 65 ms (75.56%), Space: 13.8 MB (99.13%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,7 +1,17 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         s = '1'
-        for _ in range(n - 1):
-            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        for _ in range(n-1):
+            let, temp, count = s[0], '', 0
+            for l in s:
+                if let == l:
+                    count += 1
+                else:
+                    temp += str(count)+let
+                    let = l
+                    count = 1
+            temp += str(count)+let
+            s = temp
         return s
+    
         
